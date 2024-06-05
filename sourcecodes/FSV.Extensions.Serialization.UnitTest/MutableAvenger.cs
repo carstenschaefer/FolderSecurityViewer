b45ad2dc24c8daf32ee71f4111cd06c52c248ea5
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-06-05 18:16:38 +0200

    Fixes character encoding

--- sourcecodes/FSV.Extensions.Serialization.UnitTest/MutableAvenger.cs
+++ sourcecodes/FSV.Extensions.Serialization.UnitTest/MutableAvenger.cs
@@ -1,5 +1,5 @@
 // FolderSecurityViewer is an easy-to-use NTFS permissions tool that helps you effectively trace down all security owners of your data.
-// Copyright (C) 2015 - 2024  Carsten Sch�fer, Matthias Friedrich, and Ritesh Gite
+// Copyright (C) 2015 - 2024  Carsten Schäfer, Matthias Friedrich, and Ritesh Gite
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
